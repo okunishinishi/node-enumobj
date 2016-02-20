9cb6a6bc7bb51221314e3c96de741085b364919e
Taka Okunishi <okunishitaka.com@gmail.com>
2016-02-20 09:29:53 +0900

    Add get by value

--- test/enumobj_test.js
+++ test/enumobj_test.js
@@ -8,22 +8,28 @@ const Enumobj = require('../lib/enumobj'),
     assert = require('assert');
 
 
-it('Create enumobj.', (done) => {
-    assert.ok(Enumobj);
+describe('enumobj', () => {
+    it('Create enumobj.', (done) => {
+        assert.ok(Enumobj);
 
-    var userRoles = new Enumobj({
-        ADMIN: {$val: 1, $desc: "Nothing is off limits."},
-        STAFF: {$val: 2, $desc: "Can view admin area, but not edit."},
-        CUSTOMER: {$val: 3, $desc: "Can not access admin area."}
+        let userRoles = new Enumobj({
+            ADMIN: {$val: 1, $desc: "Nothing is off limits."},
+            STAFF: {$val: 2, $desc: "Can view admin area, but not edit."},
+            CUSTOMER: {$val: 3, $desc: "Can not access admin area."}
+        });
+        assert.equal(userRoles.size(), 3);
+        assert.equal(userRoles.meta('STAFF')['$key'], 'STAFF');
+        assert.equal(userRoles.desc('ADMIN'), "Nothing is off limits.");
+        assert.equal(userRoles.get('ADMIN'), userRoles.ADMIN);
+        assert.equal(userRoles.get(1), userRoles.ADMIN);
+        assert.equal(userRoles.keyForValue(3), "CUSTOMER");
+        assert.equal(userRoles.has('admin'), false);
+        assert.equal(userRoles.has('ADMIN'), true);
+
+        assert.deepEqual(userRoles.keys(), ['ADMIN', 'STAFF', 'CUSTOMER']);
+        assert.deepEqual(userRoles.values(), [1, 2, 3]);
+        done();
     });
-    assert.equal(userRoles.size(), 3);
-    assert.equal(userRoles.meta('STAFF')['$key'], 'STAFF');
-    assert.equal(userRoles.desc('ADMIN'), "Nothing is off limits.");
-    assert.equal(userRoles.get('ADMIN'), userRoles.ADMIN);
-    assert.equal(userRoles.keyForValue(3), "CUSTOMER");
-    assert.equal(userRoles.has('admin'), false);
-    assert.equal(userRoles.has('ADMIN'), true);
-    done();
 });
 
 
