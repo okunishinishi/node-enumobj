dfb7e8f4acdd4d33a886e4e921b1e1c5914f95c4
Taka Okunishi <okunishitaka.com@gmail.com>
2016-02-24 12:59:58 +0900

    Add combine function

---
+++ test/combine_test.js
@@ -0,0 +1,33 @@
+/**
+ * Test case for combine.
+ * Runs with mocha.
+ */
+"use strict";
+
+const combine = require('../lib/combine.js'),
+    assert = require('assert');
+
+describe('combine', () => {
+
+    before((done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+
+    it('Combine', (done) => {
+        let combined = combine({
+            HOO: 'This is foo'
+        }, {
+            BAR: 'This is bar'
+        });
+        assert.deepEqual(combined.toJSON(), {
+            HOO: 'This is foo', BAR: 'This is bar'
+        });
+        done();
+    });
+});
+
